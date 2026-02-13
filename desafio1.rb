e9cdc24a527a7b4f41487e126dc3f468dacf32c6
Joao Vitor <smjvsilva@gmail.com>
2026-02-12 21:43:43 -0300

    Desafio de biblioteca: Cadastro usuario e idade

---
+++ desafio1.rb
@@ -0,0 +1,17 @@
+# Você precisa criar um programa simples de acesso a leitores dem uma biblioteca 
+# e para isso o usuário devera digitar seu nome eo sobrenome, além de sua idade
+# que é um dado relevante para o departamento de marketing daquele local. Tudo isso,
+# impresso, em uma frase.
+
+puts "--- Olá seja bem vindo a biblioteca da municipal de São Paulo! ---"
+print "Para prosseguir digite seu nome: "
+nome = gets.chomp
+
+print "Agora digite seu sobrenome:"
+sobrenome = gets.chomp
+
+print "Por fim, digite sua idade: "
+idade = gets.chomp.to_i
+
+puts "\n--- Cadastro Realizado ---"
+puts "Seja bem vindo #{nome} #{sobrenome}!\nIremos recomendar livros para faixa etária de #{idade} anos."
